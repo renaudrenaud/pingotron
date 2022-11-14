02f3a1840c519b1f40b334a99140f6791f1426c8
renaudrenaud <renaud.renaud@gmail.com>
2022-11-14 11:12:48 +0100

    Create env_var.sh
    for reference

---
+++ env_var.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# run source .vscode/env_var.sh to set the environment variables
+# -----------------------------------------------------------------
+
+echo "Define env var for PING MONITORING"
+
+export PG_URL="192.168.1.120"
+export PG_PORT="5432"
+export PG_USER="postgres"
+export PG_PASSWORD="postgres"
+export PG_DB="postgres"
+export HOSTNAME1="127.0.0.1"
+export HOSTNAME2="localhost"
